0c6f0805af6ff8e5853344c1b5eda2e07572adab
Fabio Privitera <fabio_privitera@yahoo.it>
2022-12-15 21:13:45 +0200

    DisplayNameOfDrone.js: reorganize the drone data received and fetches owners info and displays it for the final user

---
+++ frontend/components/DisplayNameOfDrone.js
@@ -0,0 +1,86 @@
+import axios from 'axios';
+import TimeStamp from './TimeStamp';
+import { useEffect, useState } from 'react';
+let shiftedDrones = [];
+let ownersAndDrones = [];
+let utilityArray = [];
+const DisplayNameOfDrone = ({ drones }) => {
+  const [pilotAndDistance, setPilotAndDistance] = useState([]);
+  useEffect(() => {
+    shiftingDrones();
+    // fetchAPI();
+  }, [drones]);
+  const shiftingDrones = () => {
+    let maxValue = drones.length;
+
+    if (maxValue > 0) {
+      let shifted = drones.shift();
+      if (!shiftedDrones.some((el) => el.idOfDrone === shifted.idOfDrone)) {
+        shiftedDrones.push(shifted);
+      } else {
+        const foundIndex = shiftedDrones.findIndex(
+          (x) => x.idOfDrone === shifted.idOfDrone
+        );
+        if (shiftedDrones[foundIndex].distance > shifted.distance) {
+          shiftedDrones[foundIndex] = shifted;
+        }
+      }
+      fetchAPI(shifted);
+    }
+  };
+  const fetchAPI = async (droneId) => {
+    const ownerInfo = await axios.get(
+      `http://localhost:5000/owners?droneID=${droneId.idOfDrone}`
+    );
+    if (!ownersAndDrones.some((el) => el.id === droneId.idOfDrone)) {
+      ownersAndDrones.push({
+        id: droneId.idOfDrone,
+        distance: droneId.distance,
+        ownerInfo: ownerInfo,
+        time: new Date(),
+      });
+    } else {
+      const index = ownersAndDrones.findIndex(
+        (x) => x.id === droneId.idOfDrone
+      );
+
+      if (ownersAndDrones[index].distance > droneId.distance) {
+        ownersAndDrones[index].distance = droneId.distance;
+      }
+    }
+
+    setPilotAndDistance(ownersAndDrones);
+    utilityArray = [...ownersAndDrones];
+
+    let dateNow = new Date();
+    for (let i = 0; i < pilotAndDistance.length; i++) {
+      let duration =
+        (dateNow.getTime() - pilotAndDistance[i].time.getTime()) / 1000;
+
+      if (duration > 40) {
+        console.log(duration);
+        utilityArray.splice(i, 1);
+        console.log(utilityArray);
+      }
+    }
+    setPilotAndDistance(utilityArray);
+  };
+
+  return (
+    <div>
+      {pilotAndDistance.map((drone) => {
+        return (
+          <div>
+            {drone.ownerInfo.data.firstName} {drone.ownerInfo.data.lastName}{' '}
+            {drone.time.getHours()}:{drone.time.getMinutes()} and closest
+            distance is = {drone.distance}, email: {drone.ownerInfo.data.email},
+            phone:{drone.ownerInfo.data.phoneNumber}
+          </div>
+        );
+      })}
+      <TimeStamp illegalDrones={pilotAndDistance}></TimeStamp>
+    </div>
+  );
+};
+
+export default DisplayNameOfDrone;
