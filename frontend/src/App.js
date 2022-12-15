9f2c6aff2cbc2c44cf89a895123107f1fb3076c2
Fabio Privitera <fabio_privitera@yahoo.it>
2022-12-15 21:15:28 +0200

    App.js contains FetchDrones.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-
+import FetchDrones from '../components/FetchDrones';
 function App() {
-  return <div className="App">hello from App</div>;
+  return (
+    <div className="App">
+      <FetchDrones></FetchDrones>
+    </div>
+  );
 }
 
 export default App;
